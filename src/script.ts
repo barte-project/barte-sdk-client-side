7c740a8a44e3db1f0673b6b3b023ff17fe7a7907
Gabriel Silva <gabsn.dev@gmail.com>
2025-08-27 15:32:32 -0300

    wip: ajustes de PCI

--- src/script.ts
+++ src/script.ts
@@ -1,41 +1,67 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const API_URL =
-    "https://bff.pcip.barte.com/service/payment/v1/sdk/card-tokens";
+const API_URL = "https://dev-bff.barte.com/service/payment/v1/sdk/card-tokens";
 
-  async function httpRequest(data: any) {
-    const bodyData = { ...data };
-    delete bodyData.accessToken;
+async function tokenizeCardRequest(data: any) {
+  const bodyData = { ...data };
+  delete bodyData.accessToken;
 
-    try {
-      const requestResult = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Token-Sdk": data.accessToken,
-        },
-        body: JSON.stringify(bodyData),
-      });
+  try {
+    const requestResult = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Token-Sdk": data.accessToken,
+      },
+      body: JSON.stringify(bodyData),
+    });
 
-      const result = await requestResult.json();
+    const result = await requestResult.json();
 
-      return {
-        error: false,
-        data: result,
-      };
-    } catch (error) {
-      return {
-        error: true,
-        errorMessage: error,
-      };
-    }
+    return {
+      error: false,
+      data: result,
+    };
+  } catch (error) {
+    return {
+      error: true,
+      errorMessage: error,
+    };
   }
+}
 
-  window.addEventListener("message", async (ev) => {
-    const eventData = ev.data;
+async function eventHandler(ev: MessageEvent<any>) {
+  const eventData = ev.data;
 
-    if (eventData.type === "submitForm") {
-      const result = await httpRequest(eventData.data);
-      window.parent.postMessage(result, "https://sdk-client.barte.com/");
-    }
-  });
+  console.log(ev.origin);
+
+  if (eventData.type === "submitForm") {
+    // const result = await tokenizeCardRequest(eventData.data);
+    const result = {
+      error: false,
+      data: {
+        token: "aksdfjaksjdf",
+      },
+    };
+
+    window.parent.postMessage(result, "http://localhost:5500/dist/parent.html");
+  }
+}
+
+const getForm = (): HTMLFormElement | null =>
+  document.getElementById("barte-tokenize-card-form") as HTMLFormElement;
+
+function handleForm() {
+  const form = getForm();
+
+  if (form) {
+    // const formData = Object.fromEntries(new FormData(form));
+    const formElements = form.elements;
+    // const elementsLength = formElements.length
+    for (const element of formElements) console.log(element);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  handleForm();
+
+  window.addEventListener("message", eventHandler);
 });
--- src/sdk.ts
+++ src/sdk.ts
@@ -1,142 +1,45 @@
-import type {
-  BarteSDKConstructorProps,
-  CardTokenData,
-  TokenizeResult,
-} from "./types";
+// TODO: Create an enum for errors
+import { BarteCardToken } from "./domain/payment/card/token";
+import type { BarteSDKConstructorProps } from "./types";
 
 export class BarteSDK {
-  private accessToken: string;
+  private cardTokenContext: BarteCardToken;
 
-  constructor({ accessToken }: BarteSDKConstructorProps) {
+  private constructor({
+    accessToken,
+    tokenizeResultCallback,
+  }: BarteSDKConstructorProps) {
     if (!window)
       throw new Error(
-        "Window is not defined, Barte SDK must be used in frontend context!"
+        "O objeto 'window' não está presente! O SDK Barte deve ser usado somente no contexto do Browser!"
       );
 
-    if (!accessToken) throw new Error("Access Token is required!");
+    if (!accessToken) throw new Error("Access Token é obrigatório!");
 
-    this.accessToken = accessToken;
-
-    // this.iframeReady = this.createIframe();
+    if (!tokenizeResultCallback || typeof tokenizeResultCallback !== "function")
+      throw new Error(
+        "Função de callback para resultado de tokenização é obrigatória"
+      );
   }
 
-  public async cardToken({
-    cardHolderName,
-    cardCVV,
-    cardExpiryDate,
-    cardNumber,
-    buyerUuid,
-  }: CardTokenData): Promise<TokenizeResult> {
-    if (cardNumber.length < 16) throw new Error("Invalid Card Number");
+  static async init(props: BarteSDKConstructorProps) {
+    // validar accessToken aqui via backend
+    const sdk = new BarteSDK(props);
+    sdk.cardTokenContext = await BarteCardToken.init({
+      accessToken: props.accessToken,
+      tokenizeResultCallback: props.tokenizeResultCallback,
+    });
+    return sdk;
+  }
 
-    if (!this.luhnValidator(cardNumber))
-      throw new Error("Invalid Card Number Format");
-
-    if (!buyerUuid) throw new Error("Invalid Buyer Uuid");
-
-    if (!cardHolderName) throw new Error("Invalid Card Holder Name");
-
-    if (!this.dateValidator(cardExpiryDate))
-      throw new Error("Invalid date format");
-
-    if (/\D/g.test(cardCVV) || cardCVV.length > 4 || cardCVV.length < 3)
-      throw new Error("Invalid Card CVV");
-
-    const iframe = await this.createIframe();
-
-    return new Promise((resolve, reject) => {
-      const listener = (message: MessageEvent<any>) => {
-        window.removeEventListener("message", listener);
-
-        if (!message.data.error) {
-          const messageData = message.data;
-
-          // TODO: Mapear outros cenários de erros possíveis aqui
-          if (Array.isArray(messageData.errors) && messageData.errors.length)
-            reject(messageData);
-
-          resolve(messageData);
-        }
-        reject(message.data);
-        this.getIFrame().remove();
-      };
-
-      window.addEventListener("message", listener);
-
-      iframe.contentWindow?.postMessage(
-        {
-          type: "submitForm",
-          data: {
-            holderName: cardHolderName.trim(),
-            cvv: cardCVV,
-            expiration: cardExpiryDate,
-            number: cardNumber,
-            buyerUuid,
-            accessToken: this.accessToken,
-          },
+  public get payment() {
+    return {
+      card: {
+        token: {
+          create: this.cardTokenContext.cardToken,
         },
-        "https://sdk-client.barte.com/script.min.js"
-      );
-    });
-  }
-
-  private getIFrame() {
-    return document.getElementById(
-      "barte-checkout-iframe"
-    ) as HTMLIFrameElement;
-  }
-
-  private createIframe(): Promise<HTMLIFrameElement> {
-    return new Promise((resolve, reject) => {
-      const iframe = document.createElement("iframe");
-      iframe.src = "https://sdk-client.barte.com";
-      iframe.id = "barte-checkout-iframe";
-      iframe.style = "display: none";
-
-      iframe.onload = () => resolve(iframe);
-
-      iframe.onerror = () => reject(new Error("Erro ao carregar iframe"));
-
-      const container = document.querySelector("body");
-      if (!container) {
-        reject(new Error("Elemento body não encontrado!"));
-        return;
-      }
-
-      container.appendChild(iframe);
-    });
-  }
-
-  private dateValidator(value: string) {
-    const expirationDateRegex = /^(0[1-9]|1[0-2])\/(\d{4})$/;
-
-    if (!expirationDateRegex.test(value)) return false;
-
-    if (Number(value.substring(3, 7)) < new Date().getFullYear()) return false;
-
-    if (Number(value.substring(0, 2)) < new Date().getMonth() + 1) return false;
-
-    return true;
-  }
-
-  private luhnValidator(number: string) {
-    const cleanNumber = number.replace(/\D/g, "");
-    let sum = 0;
-    let shouldDouble = false;
-
-    for (let i = cleanNumber.length - 1; i >= 0; i--) {
-      let digit = parseInt(cleanNumber[i]);
-
-      if (shouldDouble) {
-        digit *= 2;
-        if (digit > 9) digit -= 9;
-      }
-
-      sum += digit;
-      shouldDouble = !shouldDouble;
-    }
-
-    return sum % 10 === 0;
+      },
+    };
   }
 }
 
