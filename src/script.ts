6a54db06a12d87ac2ccca5f8a6e78679038c5b8a
Gabriel Silva <gabsn.dev@gmail.com>
2025-08-28 09:12:54 -0300

    wip: adding validation for fields

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,5 @@
+import { dateValidator, luhnValidator } from "./utils";
+
 const API_URL = "https://dev-bff.barte.com/service/payment/v1/sdk/card-tokens";
 
 async function tokenizeCardRequest(data: any) {
@@ -31,8 +33,6 @@ async function tokenizeCardRequest(data: any) {
 async function eventHandler(ev: MessageEvent<any>) {
   const eventData = ev.data;
 
-  console.log(ev.origin);
-
   if (eventData.type === "submitForm") {
     // const result = await tokenizeCardRequest(eventData.data);
     const result = {
@@ -49,14 +49,84 @@ async function eventHandler(ev: MessageEvent<any>) {
 const getForm = (): HTMLFormElement | null =>
   document.getElementById("barte-tokenize-card-form") as HTMLFormElement;
 
+const inputNames = [
+  "cardName",
+  "cardNumber",
+  "cardDueDate",
+  "cardCvv",
+] as const;
+
+type FormKeyName = (typeof inputNames)[number];
+
 function handleForm() {
   const form = getForm();
 
+  const obj = {} as Record<
+    FormKeyName,
+    { valid: boolean; errorMessage?: string }
+  >;
+
   if (form) {
-    // const formData = Object.fromEntries(new FormData(form));
     const formElements = form.elements;
-    // const elementsLength = formElements.length
-    for (const element of formElements) console.log(element);
+
+    for (const element of formElements) {
+      element.addEventListener("keyup", (e) => {
+        // @ts-ignore
+        const value = e.target.value;
+
+        // @ts-ignore
+        const name = e.target.name;
+
+        // @ts-ignore
+        const isRequired = e.target.hasAttribute("required");
+
+        if (isRequired && inputNames.includes(name)) {
+          const setSuccess = () =>
+            (obj[name as FormKeyName] = {
+              valid: true,
+              errorMessage: undefined,
+            });
+
+          const setError = (errorMessage: string) =>
+            (obj[name as FormKeyName] = {
+              valid: false,
+              errorMessage,
+            });
+
+          switch (name as FormKeyName) {
+            case "cardCvv":
+              if (value.length > 0 && value && value.length < 5) setSuccess();
+              else setError("Número de CVV inválido");
+              break;
+
+            case "cardDueDate":
+              if (dateValidator(value)) setSuccess();
+              else setError("Data de vencimento inválida");
+              break;
+
+            case "cardName":
+              if (value.length > 0) setSuccess();
+              else setError("Nome inválido");
+              break;
+
+            case "cardNumber":
+              if (luhnValidator(value)) setSuccess();
+              else setError("Número de cartão inválido");
+              break;
+          }
+        }
+
+        const entries = Object.entries(obj);
+
+        const entriesLength = entries.length;
+
+        let allEntriesAreValid = entries.every((v) => v[1].valid);
+
+        if (allEntriesAreValid && entriesLength === inputNames.length) {
+          console.log("Submit form!");
+        }
+      });
+    }
   }
 }
 
