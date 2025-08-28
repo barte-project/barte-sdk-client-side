3e24c97178e331b6806da9c3fb89df4eafcddcd2
Gabriel Silva <gabsn.dev@gmail.com>
2025-08-28 18:34:06 -0300

    chore: add log to debug

--- src/sdk.ts
+++ src/sdk.ts
@@ -55,6 +55,7 @@ export class BarteSDK {
           if (Array.isArray(messageData.errors) && messageData.errors.length)
             reject(messageData);
 
+          console.log({ messageData });
           resolve(messageData);
         }
         reject(message.data);
